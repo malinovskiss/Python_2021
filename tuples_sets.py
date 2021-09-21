58331cd57cb26cd50eb0e89cf8356a67cec61ab3
VVSprogramm <vvsprogrammesana@gmail.com>
2021-09-21 15:38:54 +0300

    tuples and sets

---
+++ tuples_sets.py
@@ -0,0 +1,20 @@
+#Tuples
+
+# ()
+
+my_tuple = ('ēst', 'gulēt', 'slinkot')
+
+print(len(my_tuple))
+
+print(my_tuple[1])
+
+print(my_tuple.index('ēst'))
+
+my_tuple = (11,11,12,11,13,11)
+
+print(my_tuple.count(11))
+
+
+#Set
+
+# {}
