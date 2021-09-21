e9b852f8e807895f044aadb9dc341ceec89ca386
Programmēšana VVS <74134370+VVSprogramm@users.noreply.github.com>
2021-09-21 09:21:07 +0000

    add string.py

--- piemers.py
+++
@@ -1,3 +0,0 @@
-print("Hey!")
-
-print("Hey, Hey!")
---
+++ string.py
@@ -0,0 +1,34 @@
+'''
+Mainīgais string
+Teksta Mainīgais
+a = 'teksts'
+a = "teksts"
+'''
+
+#Tiek definēti mainīgie
+name = "Hello"
+name2 = "World"
+
+#Mainīgo izvade
+print(name)
+print(name2)
+
+print(name + ' ' + name2)
+
+print(name, name2)
+
+print(name, end = " ")
+print(name2)
+
+#mainīgā garums
+print(len(name))
+
+name = "WorldHello"
+
+print(name)
+
+#Izvadam konkrētu simbolu, lietojam []
+print("name[0]: ", name[0])
+
+print("name[2:5]", name[1:5])
+
